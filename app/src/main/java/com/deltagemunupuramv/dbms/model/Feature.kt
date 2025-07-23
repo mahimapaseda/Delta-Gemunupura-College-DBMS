ed429c6d7964bbba67522886de6fa597f7aa0b03
mahimapaseda <mahimapasedakusumsiri@gmail.com>
2025-07-23 18:16:59 +0530

    2nd search

--- app/src/main/java/com/deltagemunupuramv/dbms/model/Feature.kt
+++
@@ -1,9 +0,0 @@
-package com.deltagemunupuramv.dbms.model
-
-data class Feature(
-    val icon: Int,
-    val title: String,
-    val description: String,
-    val canView: Boolean = true,
-    val canModify: Boolean = true
-) 
--- app/src/main/java/com/deltagemunupuramv/dbms/model/TestConnection.kt
+++
@@ -1,7 +0,0 @@
-package com.deltagemunupuramv.dbms.model
-
-data class TestConnection(
-    val id: String = "",
-    val message: String = "",
-    val timestamp: Long = System.currentTimeMillis()
-) 
